3faa4d9c7a91b54359621a9c21a4c05352d743eb
chnn10 <46521173+chnn10@users.noreply.github.com>
2022-06-23 09:52:10 +0800

    Update Demo3EveryBatchTest.java

--- CodePractice/Java/Demo3EveryBatchTest.java
+++ CodePractice/Java/Demo3EveryBatchTest.java
@@ -1,9 +1,5 @@
 
-package com.huawei.csb.cbccrmbusinessmgmtservice.business.demo;
-
 import org.apache.commons.collections4.ListUtils;
-import org.apache.kafka.common.protocol.types.Field;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -14,7 +10,7 @@ import java.util.Map;
  * 一个list进去，返回一个map，这个list超过了100，需要进行分批处理
  */
 public class Demo5EveryBatchTest {
-    public Map<Integer,Integer> getBatchValue(List<Integer> stringList) {
+    public static Map<Integer,Integer> getBatchValue(List<Integer> stringList) {
 
         if (stringList.size() >= 100) {
 
@@ -38,14 +34,27 @@ public class Demo5EveryBatchTest {
 
     }
 
-    private Map<Integer,Integer> getValue(List<Integer> list) {
+    private static Map<Integer,Integer> getValue(List<Integer> list) {
         Map<Integer,Integer> map = new HashMap<>();
         for (int i = 0; i < list.size(); i++) {
             map.put(list.get(i), list.get(i));
         }
         return map;
     }
+
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0;i <= 200; i++) {
+            list.add(i);
+        }
+        Map<Integer,Integer> map = getBatchValue(list);
+        map.forEach((k,v) ->{
+            System.out.println("k: "+k+", v: "+v);
+        });
+    }
 }
+
+
 
 
 
